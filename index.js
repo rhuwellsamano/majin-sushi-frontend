fb12ee8ecff8d8b33848fa09a337c6756432f464
rhuwellsamano <rhuwell.samano@gmail.com>
2019-03-06 00:04:17 -0500

    bunch of css and sound stuff

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// AUDIO
+
+var audio1 = document.getElementById("sushi-mouseover");
+var audio2 = document.getElementById("button-mouseover");
+var audio3 = document.getElementById("add-sushi-to-order-click");
+var audio4 = document.getElementById("remove-button-click");
+var audio5 = document.getElementById("remove-item-from-order-click");
+var audio6 = document.getElementById("checkout-click");
+
+function playSushiMouseover() {
+  audio1.play();
+}
+
+function playButtonMouseover() {
+  audio2.play();
+}
+
+function playAddSushiToOrderClick() {
+  audio3.play();
+}
+
+function playRemoveButtonClick() {
+  audio4.play();
+}
+
+function playRemoveItemFromOrderClick() {
+  audio5.play();
+}
+
+function playCheckoutClick() {
+  audio6.play();
+}
+
+// END OF AUDIO - might move to separate JS file
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -12,13 +46,17 @@ function showSushi(sushi) {
   let sushiMenu = document.querySelector('#sushi-menu')
    sushiMenu.innerHTML += `
       <div class="sushi">
-       <h3 class="sushi_title">${sushi.name}</h3>
-       <img src="${sushi.image_url}" class="grow grow:hover">
-       <p class="sushi_description">
+        <div class="shine shine:hover">
+       <div><span><h3 class="sushi-title button-grow button-grow:hover go-left">${sushi.name}</h3><h3 class="sushi-price button-grow button-grow:hover go-right">$${sushi.price}.00</h3></span></div>
+       <br>
+       <br>
+       <div><img src="${sushi.image_url}" class="sushi-image grow grow:hover wobble" onmouseover=playSushiMouseover() onclick=playAddSushiToOrderClick()></div>
+       <div class="sushi-description right">
+        <p>
          ${sushi.description}
        </p>
-       <button class="button" data-id=${sushi.id}>Add to Order</button>
-       <button class"remove-button">Remove Item</button>
+       <p><button class="button add-to-order-button shine shine:hover button-grow button-grow:hover" data-id=${sushi.id} onmouseover=playButtonMouseover() onclick=playAddSushiToOrderClick()>+</button><button class="remove-from-menu-button button-grow button-grow:hover" data-id=${sushi.id} onmouseover=playButtonMouseover() onclick=playRemoveButtonClick()>-</button></p>
+       </div>
        </div>
        `
 }
@@ -33,6 +71,9 @@ getSushis().then(sushiObj => {
     showSushi(sushi)
   })
 })
+
+
+
 
 
 
